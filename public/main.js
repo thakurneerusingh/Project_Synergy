e196e879ef1636e10807fd105f677b755b4bf940
Neeru <107040990+thakurneerusingh@users.noreply.github.com>
2022-06-23 16:53:53 +0530

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,33 @@
+
+  $(function(){
+     $("#main_form").on('submit',function(e){
+         e.preventDefault();
+         $.ajex({
+             url:$(this).attr('action'),
+             method:$(this).attr('method'),
+             data:new FormData(this),
+             processData:false,
+             dataType:'json',
+             contentType:false,
+            
+             beforeSend:function(){
+                 $(document).find('span.error-text').text('');
+             },
+
+             success:function(data){
+                 if(data.status == 0){
+                     $.echo(data.error, function(prefix, val){
+                             $('span.' +prefix+'_error').text(val[0]);
+                     });
+
+                 }
+                 else{
+                     $('#main_form')[0].reset();
+                     alert(data.msg);
+                 }
+
+             }
+         });
+     });
+});
+    
